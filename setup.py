0bb4bfc3be20b94e107d7ce6903c898e714987ff
jchemutt <jchemutt@users.noreply.github.com>
2024-11-08 22:33:43 +0000

    Update version to v1.0.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="modelswp",
-    version='v1.0.13',
+    version='v1.0.14',
     author="CarlosNasayo",
     author_email="c.nasayo@cgiar.com",
     description="Modelos para la base de datos waterpoints",
